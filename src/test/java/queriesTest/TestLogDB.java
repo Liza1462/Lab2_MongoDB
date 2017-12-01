320f37a6a7064931a4adf3217bc19b3ee2dbcb5b
Liza Bikhanova <L146262@gmail.com>
2017-12-01 04:47:29 +0200

    edit test

--- src/test/java/queriesTest/TestLogDB.java
+++ src/test/java/queriesTest/TestLogDB.java
@@ -26,24 +26,48 @@ public class TestLogDB {
     public static  MongoCollection<org.bson.Document> logs = testdb.getCollection("testLogs1");
     public static DBCollection logs2 = testdb.getCollectionMR("testLogs1");
 
+    public static void insert() throws IOException{
+        logs.drop();
+        String s1 = "{\"ip\":\"78.123.67.3\",\"url\":\"https://en.wikipedia.org/wiki/Main_Page\"," +
+                "\"timeStamp\":\"Nov 26, 2017 12:24:43 AM\",\"timeSpent\":200}\n";
+        String s2 = "{\"ip\":\"157.55.39.105\",\"url\":\"https://en.wikipedia.org/wiki/Nikita_Khrushchev\"" +
+                ",\"timeStamp\":\"Nov 26, 2017 1:20:39 AM\",\"timeSpent\":600}\n";
+        String s3 ="{\"ip\":\"40.77.167.48\",\"url\":\"https://en.wikipedia.org/wiki/Kim_Kardashian\"" +
+                ",\"timeStamp\":\"Nov 27, 2017 1:00:25 PM\",\"timeSpent\":678}\n";
+        String s4 = "{\"ip\":\"40.77.167.48\",\"url\":\"https://en.wikipedia.org/wiki/Main_Page\",\"" +
+                "timeStamp\":\"Nov 26, 2017 12:24:45 AM\",\"timeSpent\":378}\n";
+        String s5 = "{\"ip\":\"157.55.39.105\",\"url\":\"https://en.wikipedia.org/wiki/Soviet_Union\",\"" +
+                "timeStamp\":\"Nov 26, 2017 1:05:29 AM\",\"timeSpent\":780}";
+        String s6 = "{\"ip\":\"40.77.167.48\",\"url\":\"https://en.wikipedia.org/wiki/Soviet_Union\",\"" +
+                "timeStamp\":\"Nov 26, 2017 12:30:45 AM\",\"timeSpent\":1000}\n";
+
+//        List<String> inStr = new ArrayList<>(Arrays.asList(s1, s2, s3, s4, s5, s6));
+
+        logs.insertOne(org.bson.Document.parse(s1));
+        logs.insertOne(org.bson.Document.parse(s2));
+        logs.insertOne(org.bson.Document.parse(s3));
+        logs.insertOne(org.bson.Document.parse(s4));
+        logs.insertOne(org.bson.Document.parse(s5));
+        logs.insertOne(org.bson.Document.parse(s6));
+    }
+
     @Test
     public void testInsert() throws IOException{
         logs.drop();
         logs2.drop();
 
-       LogsDB.insertFromJson("files/testRes", logs);
+        LogsDB.insertFromJson("files/testRes", logs);
 
         List <String> res = new ArrayList<>();
         for (Document log : logs.find()){
             res.add(log.toJson());
         }
-
         assertEquals(res.size(), 6);
     }
 
     @Test
     public void testGetIP() throws IOException{
-//        insert();
+        insert();
         List <String> result = LogsDB.getIPs("https://en.wikipedia.org/wiki/Main_Page", logs);
         assertEquals( Arrays.asList("40.77.167.48", "78.123.67.3"), result);
     }
@@ -94,5 +118,9 @@ public class TestLogDB {
 //        rightRes.add("https://en.wikipedia.org/wiki/Soviet_Union date: Sun Nov 26 2017 count: 2.0");
 //        assertEquals(rightRes, LogsDB.getURLMR(logs2));
 //    }
+
+
+
+
 
 }
